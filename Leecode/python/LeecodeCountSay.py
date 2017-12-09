7e87d95baacea9c87cd2b78ad0510dc23d9948bc
dufufeisdu <fufei.du@gmail.com>
2017-12-09 11:08:23 -0800

    (change)refine while loop

--- Leecode/python/LeecodeCountSay.py
+++ Leecode/python/LeecodeCountSay.py
@@ -3,6 +3,7 @@
 # mistake 0: pathlogic case
 # mistake 1: string is not changable
 # mistake 2: should use try and except to handle out range
+# improve 3: could use a label and not use two while loop
 
 
 def count_and_say(n):
@@ -19,27 +20,27 @@ def count_and_say(n):
         len_numb = len(numb)
         y = 0
         numb_c = ''
+        flag_enter = True
+        count = 0
         while y < len_numb:
-            start = y
-            count = 1
-            while True:
-                try:
-                    temp = numb[y + 1]
-                    if temp == numb[y]:
-                        count += 1
-                        y += 1
-                    else:
-                        numb_c += str(count)
-                        numb_c += numb[start]
-                        y += 1
-                        break
-                except IndexError:
-                    y += 1
+            count += 1
+            if flag_enter:
+                if len_numb == y + 1:
                     numb_c += str(count)
-                    numb_c += numb[start]
+                    numb_c += numb[y]
                     break
+                elif numb[y + 1] != numb[y]:
+                    numb_c += str(count)
+                    numb_c += numb[y]
+                    flag_enter = False
+
+            else:
+                count = 0
+                flag_enter = True
+                y -= 1
+            y += 1
         numb = numb_c
     return numb
 
 
-print(count_and_say(6))
+print(count_and_say(7))
